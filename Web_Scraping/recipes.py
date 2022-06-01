23ecaac0db4e6cb278101500ae75231f94329284
Matt Adams <mattaadams1@gmail.com>
2022-05-31 23:25:57 -0400

    loop

--- Web_Scraping/recipes.py
+++ Web_Scraping/recipes.py
@@ -1,20 +1,29 @@
+import csv
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+import re
+
+MAIN_STRING = "carouselNav__list elementFont__resetList recipeCarousel__list"  # class for main recipe category page
+CAT_STRING = "category-page-list-content category-page-list-content__recipe-card karma-main-column"  # Class for categorie
+POPULAR_STRING = "card__titleLink manual-link-behavior elementFont__title margin-8-bottom"  # Class for most popular recipes
 
 source = requests.get('https://www.allrecipes.com/recipes/').text
-
 soup = BeautifulSoup(source, 'lxml')
-
-recipe_list = soup.find('ul', class_="carouselNav__list elementFont__resetList recipeCarousel__list")
+recipe_list = soup.find('ul', class_=MAIN_STRING)
 
 links = []
+pop_recipes = []
 for a in recipe_list.find_all('a', href=True):
     links.append(a['href'])
+    for i, _ in enumerate(links):
+        recipe_category = requests.get(links[i]).text
+        cat_soup = BeautifulSoup(recipe_category, 'lxml')  # category page
+        most_popular = cat_soup.find('div', class_=CAT_STRING)
+        for a in most_popular.find_all('a', class_=POPULAR_STRING, href=True):
+            if re.search('https://www.allrecipes.com/recipe/.*', a['href']):
+                pop_recipes.append(a['href'])
 
-# print(links[0])
 
-recipe_category = requests.get(links[0]).text
-
-cat_soup = BeautifulSoup(recipe_category, 'lxml')
-print(cat_soup.prettify())
+df = pd.DataFrame(pop_recipes, columns=["link"])
+df.to_csv('list.csv', index=False)
