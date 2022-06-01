820b8f82ca53d99f12b4f62bc9cc67b3db80f6b8
Matt Adams <mattaadams1@gmail.com>
2022-05-31 23:50:38 -0400

    cleanup

--- Web_Scraping/recipes.py
+++ Web_Scraping/recipes.py
@@ -4,26 +4,28 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-MAIN_STRING = "carouselNav__list elementFont__resetList recipeCarousel__list"  # class for main recipe category page
-CAT_STRING = "category-page-list-content category-page-list-content__recipe-card karma-main-column"  # Class for categorie
-POPULAR_STRING = "card__titleLink manual-link-behavior elementFont__title margin-8-bottom"  # Class for most popular recipes
-
-source = requests.get('https://www.allrecipes.com/recipes/').text
-soup = BeautifulSoup(source, 'lxml')
-recipe_list = soup.find('ul', class_=MAIN_STRING)
-
-links = []
-pop_recipes = []
-for a in recipe_list.find_all('a', href=True):
-    links.append(a['href'])
-    for i, _ in enumerate(links):
-        recipe_category = requests.get(links[i]).text
-        cat_soup = BeautifulSoup(recipe_category, 'lxml')  # category page
-        most_popular = cat_soup.find('div', class_=CAT_STRING)
-        for a in most_popular.find_all('a', class_=POPULAR_STRING, href=True):
-            if re.search('https://www.allrecipes.com/recipe/.*', a['href']):
-                pop_recipes.append(a['href'])
+MAIN_STRING = "carouselNav__list elementFont__resetList recipeCarousel__list"  # class for main recipe category page navlist
+CAT_STRING = "category-page-list-content category-page-list-content__recipe-card karma-main-column"  # Class for category page div
+CARDS_STRING = "card__titleLink manual-link-behavior elementFont__title margin-8-bottom"  # Class for most popular recipe cards
 
 
-df = pd.DataFrame(pop_recipes, columns=["link"])
-df.to_csv('list.csv', index=False)
+def get_links():
+    source = requests.get('https://www.allrecipes.com/recipes/').text
+    soup = BeautifulSoup(source, 'lxml')
+    recipe_list = soup.find('ul', class_=MAIN_STRING)
+
+    links = []
+    pop_recipes = []
+    for a in recipe_list.find_all('a', href=True):
+        links.append(a['href'])
+        for i, _ in enumerate(links):
+            recipe_category = requests.get(links[i]).text
+            cat_soup = BeautifulSoup(recipe_category, 'lxml')  # category page
+            most_CARDS = cat_soup.find('div', class_=CAT_STRING)
+            for a in most_CARDS.find_all('a', class_=CARDS_STRING, href=True):
+                if re.search('https://www.allrecipes.com/recipe/.*', a['href']):
+                    pop_recipes.append(a['href'])
+
+    df = pd.DataFrame(pop_recipes, columns=["link"])
+    df.to_csv('list.csv', index=False)
+    return df
