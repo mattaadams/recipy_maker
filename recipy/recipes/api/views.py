e1a4d0433bb851d42243b1ce0e54a38a7f9a85a0
Matt Adams <mattaadams1@gmail.com>
2022-07-17 09:34:30 -0400

    swagger tags

--- recipy/recipes/api/views.py
+++ recipy/recipes/api/views.py
@@ -1,4 +1,5 @@
 
+from drf_yasg.utils import swagger_auto_schema
 
 from rest_framework.generics import (
     CreateAPIView,
@@ -32,14 +33,24 @@ from .serializers import (
 
 
 class RecipeListAPIView(ListAPIView):
+
     queryset = Recipe.objects.all()
     serializer_class = RecipeListSerializer
 
+    @swagger_auto_schema(tags=['Recipes'])
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
 
 class RecipeCreateAPIView(CreateAPIView):
+
     queryset = Recipe.objects.all()
     serializer_class = RecipeCreateUpdateSerializer
     permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(tags=['Recipes'])
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -48,6 +59,10 @@ class RecipeCreateAPIView(CreateAPIView):
 class RecipeDetailAPIView(RetrieveAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeDetailSerializer
+
+    @swagger_auto_schema(tags=['Recipes'])
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 
 class RecipeUpdateAPIView(RetrieveUpdateAPIView):
@@ -58,22 +73,50 @@ class RecipeUpdateAPIView(RetrieveUpdateAPIView):
     def perform_update(self, serializer):
         serializer.save(author=self.request.user)
 
+    @swagger_auto_schema(tags=['Recipes'])
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Recipes'])
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Recipes'])
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
 
 class RecipeDeleteAPIView(RetrieveDestroyAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeDetailSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
+    @swagger_auto_schema(tags=['Recipes'])
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Recipes'])
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
 
 class IngredientListAPIView(ListAPIView):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientListSerializer
+
+    @swagger_auto_schema(tags=['Ingredients'])
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
 
 class IngredientCreateAPIView(CreateAPIView):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientCreateUpdateSerializer
     permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(tags=['Ingredients'])
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -88,6 +131,10 @@ class IngredientDetailAPIView(RetrieveAPIView):
         print(obj.__dict__)
         return obj
 
+    @swagger_auto_schema(tags=['Ingredients'])
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
 
 class IngredientUpdateAPIView(RetrieveUpdateAPIView):
     queryset = Ingredient.objects.all()
@@ -97,11 +144,31 @@ class IngredientUpdateAPIView(RetrieveUpdateAPIView):
     def perform_update(self, serializer):
         serializer.save(author=self.request.user)
 
+    @swagger_auto_schema(tags=['Ingredients'])
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Ingredients'])
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Ingredients'])
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
 
 class IngredientDeleteAPIView(RetrieveDestroyAPIView):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientDetailSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsParentOwnerOrReadOnly]
+
+    @swagger_auto_schema(tags=['Ingredients'])
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Ingredients'])
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
     def get_object(self):
         obj = super().get_object()
@@ -113,16 +180,29 @@ class CommentListAPIView(ListAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentListSerializer
 
+    @swagger_auto_schema(tags=['Comments'])
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
 
 class CommentCreateAPIView(CreateAPIView):
+
     queryset = Comment.objects.all()
     serializer_class = CommentCreateUpdateSerializer
     permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(tags=['Comments'])
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
 class CommentDetailAPIView(RetrieveAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentDetailSerializer
+
+    @swagger_auto_schema(tags=['Comments'])
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 
 class CommentUpdateAPIView(RetrieveUpdateAPIView):
@@ -130,8 +210,29 @@ class CommentUpdateAPIView(RetrieveUpdateAPIView):
     serializer_class = CommentCreateUpdateSerializer
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(tags=['Comments'])
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Comments'])
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Comments'])
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
 
 class CommentDeleteAPIView(DestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentDetailSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]\
+
+
+    @swagger_auto_schema(tags=['Comments'])
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Comments'])
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
--- recipy/users/api/views.py
+++ recipy/users/api/views.py
@@ -1,3 +1,5 @@
+from drf_yasg.utils import swagger_auto_schema
+
 from rest_framework.generics import (
     CreateAPIView,
     ListAPIView,
@@ -25,7 +27,15 @@ class UserListAPIView(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserListSerializer
 
+    @swagger_auto_schema(operation_description="Return a list of users", tags=['Users'])
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
 
 class UserDetailAPIView(RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserDetailSerializer
+
+    @swagger_auto_schema(tags=['Users'])
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
