b741b716e37f1b2a7b471305d35392621dab8b8f
Matt Adams <mattaadams1@gmail.com>
2022-07-18 18:10:41 -0400

    nested serializer

--- recipy/recipes/api/serializers.py
+++ recipy/recipes/api/serializers.py
@@ -3,29 +3,18 @@ from recipes.models import Recipe, Ingredient, Comment
 
 
 class IngredientListSerializer(serializers.ModelSerializer):
-    author = serializers.CharField(source="recipe.author", read_only=True)
 
     class Meta:
         model = Ingredient
-        fields = [
-            'id',
-            'name',
-            'recipe',
-            'author'
-
-        ]
+        fields = ['name']
 
 
 class IngredientCreateUpdateSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(required=False)
+    id = serializers.IntegerField(required=False, write_only=False)
 
     class Meta:
         model = Ingredient
-        fields = [
-            'id',
-            'name',
-
-        ]
+        fields = ['id', 'name']
 
 
 class IngredientDetailSerializer(serializers.ModelSerializer):
@@ -64,7 +53,7 @@ class RecipeListSerializer(serializers.ModelSerializer):
 class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
     ingredients = IngredientCreateUpdateSerializer(many=True)
     author = serializers.CharField(read_only=True)
-    image_url = serializers.URLField(required=False, allow_blank=True, default=None)
+    image_url = serializers.URLField(required=False, allow_null=True)
 
     class Meta:
 
@@ -87,21 +76,35 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
         return recipe
 
     def update(self, instance, validated_data):
-        ingredients_data = validated_data.pop("ingredients")
 
-        remove_items = {item.id: item for item in instance.ingredients.all()}
+        ingredients_data = validated_data.pop("ingredients")   # updated data
+        print(ingredients_data)
+        remove_items = {item.id: item for item in instance.ingredients.all()}  # old data
 
-        for ingredient_data in ingredients_data:
+        for i, ingredient_data in enumerate(ingredients_data):
             item_id = ingredient_data.get("id", None)
 
             if item_id is None:
                 # new item so create this
                 instance.ingredients.create(**ingredient_data)
             elif remove_items.get(item_id, None) is not None:
+
                 # update this item
                 instance_item = remove_items.pop(item_id)
 
                 Ingredient.objects.filter(id=instance_item.id).update(**ingredient_data)
+            else:
+                # we replace the user provided id with old one.. as we don't want it to be modified...
+                # I'm not sure how else to handle as in order to get ingredients id in validated data it has to be writable
+                # At least this prevents it from being changed even if user can provide an input
+                if len(list(remove_items)) > 0:
+                    old_id = list(remove_items.keys())[0]
+                    ingredient_data['id'] = old_id
+                    remove_items.pop(old_id)
+                    Ingredient.objects.filter(id=old_id).update(**ingredient_data)
+                else:
+                    ingredient_data.pop('id')
+                    instance.ingredients.create(**ingredient_data)
 
         for item in remove_items.values():
             item.delete()
@@ -116,6 +119,7 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
 class RecipeDetailSerializer(serializers.ModelSerializer):
     ingredients = IngredientListSerializer(many=True, read_only=True)
     author = serializers.CharField(read_only=True)
+    favorite_count = serializers.IntegerField(source='favorites.count', read_only=True)
 
     class Meta:
 
@@ -127,7 +131,9 @@ class RecipeDetailSerializer(serializers.ModelSerializer):
             'description',
             'ingredients',
             'instructions',
-            'date_posted'
+            'date_posted',
+            'favorites',
+            'favorite_count'
         ]
 
 
