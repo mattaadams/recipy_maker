c37627694b638f1dd6f419dcd3b2acf23cfdf458
Matt Adams <mattaadams1@gmail.com>
2022-08-28 18:49:59 -0400

    file removal, filetype valid

--- recipy/users/validators.py
+++ recipy/users/validators.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 
 def validate_file_extension(value):
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
-    valid_extensions = ['.jpg', '.png']
+    valid_extensions = ['.jpg', '.png','.jpeg']
     if not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file extension.')
 
